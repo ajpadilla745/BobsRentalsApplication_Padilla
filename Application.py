399a870ca6166dd74ddb6d032341a0767a2c1db6
ajpadilla745 <ajpadilla@cincinnatistate.edu>
2026-08-13 18:21:08 -0400

    The rental returns is completed. Therefore, can keep track of revenue and returned inventory.

--- Application.py
+++ Application.py
@@ -1,7 +1,7 @@
 #=======================================================
 # Name: Antonio Padilla
 # Course: Object-Oriented Programming
-# Assignment: Final Project PT.2
+# Assignment: Final Project Pt.2
 #=======================================================
 from customer import Customer
 from rental import Rental
@@ -29,12 +29,14 @@ def customer_rental():
 
             print("Enter the customer's name")
             customer_input = input("-> ")
-            if customer_input != "":
-                print("Enter customer ID")
+            if customer_input == "":
+                raise Exception("Invalid name")
+            print("Enter customer ID")
             id_input = int(input("->"))
             id_input = ValidateIntegerInput(id_input)
             if id_input != "":
-                customer_object = Customer(customer_id = id_input, name = customer_input)
+                global customer_obj
+                customer_obj = Customer(customer_id = id_input, name = customer_input)
             print("Select a rental period")
             print("1.hourly")
             print("2.daily")
@@ -44,16 +46,22 @@ def customer_rental():
                 print("How many hours?")
                 duration_input = int(input("-> "))
                 duration_input = ValidateIntegerInput(duration_input)
-            if duration_input > 0 and duration_input < 24:
-                hourly_rental = Rental(customer = customer_object, rental_period_type = rental_input, duration = duration_input)
-                if rental_input == "daily":
+                if duration_input > 0 and duration_input < 24:
+                    global hourly_rental
+                    hourly_rental = Rental(customer = customer_obj, rental_period_type = rental_input, duration = duration_input)
+            elif rental_input == "daily":
+                    global daily_rental
                     daily_rental = Rental(Customer, rental_period_type = rental_input, duration = 1)
-                    if rental_input == "weekly":
+            elif rental_input == "weekly":
+                        global weekly_rental
                         weekly_rental = Rental(Customer, rental_period_type = rental_input, duration = 1)
             else:
                 raise Exception("invalid input")
+            
             while True:
-                print("Skis or Snowboards? ")
+                print("Select a rental type")
+                print("1. Skis")
+                print("2. Snowboards")
                 equipment_input = input("-> ")
                 if equipment_input == "Skis": 
                     print("Select Ski brand")
@@ -69,13 +77,10 @@ def customer_rental():
                         ski_item = Ski(equipment_id= "SKI-203", brand = "Rossignol")
                     if rental_input == "hourly":
                         hourly_rental.add_item(ski_item) 
-                        rent_shop.check_and_rent_inventory(hourly_rental)
                     elif rental_input == "daily":
                         daily_rental.add_item(ski_item) 
-                        rent_shop.check_and_rent_inventory(daily_rental)
                     elif rental_input == "weekly":
-                        weekly_rental.additem(ski_item)
-                        rent_shop.check_and_rent_inventory(weekly_rental)
+                        weekly_rental.add_item(ski_item)
 
                 if equipment_input == "Snowboards":
                     print("Select Snowboard brand")
@@ -83,60 +88,102 @@ def customer_rental():
                     print("2. Burton")
                     print("3. Rossignol")
                     equipment_input = input("-> ")
-                if equipment_input == "Salomon":
+                    if equipment_input == "Salomon":
                         snowboard_item = Snowboard(equipment_id= "SBD-201", brand = "Salomon")
-                elif equipment_input == "Burton":
-                    snowboard_item = Snowboard(equipment_id= "SBD-202", brand = "Burton")
-                elif equipment_input == "Rossignol":
-                    snowboard_item = Snowboard(equipment_id= "SBD-203", brand = "Burton")
-                if rental_input == "hourly":
+                    elif equipment_input == "Burton":
+                        snowboard_item = Snowboard(equipment_id= "SBD-202", brand = "Burton")
+                    elif equipment_input == "Rossignol":
+                        snowboard_item = Snowboard(equipment_id= "SBD-203", brand = "Rossignol")
+                    if rental_input == "hourly":
                         hourly_rental.add_item(snowboard_item) 
-                        hourly_cost = hourly_rental.calculate_best_item_price 
-                elif rental_input == "daily":
-                    daily_rental.add_item(snowboard_item) 
-                    daily_cost = daily_rental.calculate_best_item_price 
-                elif rental_input == "weekly":
-                    weekly_rental.additem(snowboard_item)
-                    weekly_cost = weekly_rental.calculate_best_item_price 
-                print("finished? Y/N")
+                    elif rental_input == "daily":
+                        daily_rental.add_item(snowboard_item)
+                    elif rental_input == "weekly":
+                        weekly_rental.add_item(snowboard_item)
+
+
+
+                print("Complete rental? Y/N")
                 finish_input = input("->")
                 if finish_input == "Y":
-                    print("Have a promocode? Y/N")
-                    finish_input = input("-> ")
-                    if finish_input == "Y":  
-                            print("Enter a promo code")
-                            promo_input = input("->")
-                    else:
-                        promo_input == ""           
+                    print("Enter a promo code")
+                    global promo_input
+                    promo_input = input("->")         
                     if rental_input == "hourly":
                         grand_total = hourly_rental.calculate_total_bill(promo_input)
+                        rent_shop.check_and_rent_inventory(hourly_rental)
                         print("")
                         print(f"Total: ${grand_total:.2f}")
                         print("")
                     elif rental_input == "daily":
                         grand_total = daily_rental.calculate_total_bill(promo_input)
+                        rent_shop.check_and_rent_inventory(daily_rental) 
                         print("")
                         print(f"Total: ${grand_total:.2f}")
                         print("")
                     elif rental_input == "weekly":
                         grand_total = weekly_rental.calculate_total_bill(promo_input)
-                        print("=" * 30)
+                        rent_shop.check_and_rent_inventory(weekly_rental)
+                        print("")
                         print(f"Total: ${grand_total:.2f}")
-                        print("=" * 30)
+                        print("")
+                    print("")
+                    print("The rental is complete")
+                    print("")
                     break
         except Exception as ex :
             print(f"An error has occurred, {ex=}, {type(ex)=}")
         
 
-          
-#def rental_return():
-
+#This is to process returns for any rental equipment and a bill to collect revenue    
+def rental_return():
+    while True:
+        try:
+            if rent_shop.total_skis_rented_today == 0 and rent_shop.total_snowboards_rented_today == 0:
+                print("")
+                print("No rentals have been made today")
+                print("")
+                break
+            else:
+                print("Enter the customer's name")
+                customer_input = input("-> ")
+                if customer_input != "":
+                    print("Enter customer ID")
+                id_input = int(input("->"))
+                id_input = ValidateIntegerInput(id_input)
+                if id_input != "":
+                    if id_input == customer_obj.customer_id and customer_input == customer_obj.name:
+                        print("Customer has been found!")
+                else:
+                 print("No rentals have been made by this person/group")
+                 break
+                print(f"Name: {customer_obj.name}")
+                print(f"ID: {customer_obj.customer_id}")
+                if hourly_rental.rental_period_type == "hourly":
+                    print(f"Rental Period: {hourly_rental.rental_period_type}")
+                    print("Processing return...")
+                    rent_shop.process_return_to_inventory(hourly_rental, promo_input)
+                elif daily_rental.rental_period_type == "daily":
+                    print(f"Rental Period: {daily_rental.rental_period_type}")
+                    print("Processing return...")
+                    rent_shop.process_return_to_inventory(daily_rental, promo_input)
+                elif weekly_rental.rental_period_type == "weekly":
+                    print(f"Rental Period: {weekly_rental.rental_period_type}")
+                    print("Processing return...")
+                    rent_shop.process_return_to_inventory(weekly_rental, promo_input)
+                print("Return was successful")
+            break
+        except Exception as ex :
+            print(f"An error has occurred, {ex=}, {type(ex)=}")
+               
+#Shows the current inventory available for rent
 def show_inventory():
      print("=" * 65)
      print("Total amount of skis during the day:", rent_shop._available_skis)
      print("Total amount of snowboards during the day:", rent_shop._available_snowboards)
      print("=" * 65)
 
+#A report calculating all the skis, snowboards, and revenue collected for the day.
 def end_of_day_report():
     print("=" * 65)
     print("                       End of Day Report                      ")
@@ -144,13 +191,8 @@ def end_of_day_report():
     print("")
     print("Total skis rented:", rent_shop.total_skis_rented_today)
     print("Total Snowboards rented:", rent_shop.total_snowboards_rented_today)
-    print("Total daily revenue:", rent_shop.total_revenue_collected_today)
+    print(f"Total daily revenue: {rent_shop.total_revenue_collected_today:.2f}")
     print("=" * 65)
-
-
-
-#Variables
-promo_validation = False
 
 
 #The boot up sequence
@@ -162,6 +204,7 @@ number_of_snowboards = int(input("Enter the number of snowboards: "))
 number_of_snowboards = ValidateIntegerInput(number_of_snowboards)
 rent_shop = RentalShop(starting_skis = number_of_skis, starting_snowboards = number_of_snowboards)
 
+#The main menu used to navigate through
 print("Starting main menu...")
 print("=" * 65)
 print("                 Bob's Ski & Snowboard Rentals               ")
@@ -175,13 +218,13 @@ while True:
     print("4. End of Day")
     menu_input = input("-> ")
 
-    if menu_input == "New Customer Rental":
+    if menu_input == "1":
         customer_rental()
-    #elif menu_input == "Rental Return":
-           #rental_return()
-    elif menu_input == "Show Inventory":
+    elif menu_input == "2":
+           rental_return()
+    elif menu_input == "3":
            show_inventory()
-    elif menu_input == "End of Day":
+    elif menu_input == "4":
            end_of_day_report()
            break
 
--- README.md
+++ README.md
@@ -5,34 +5,39 @@ Name: Antonio Padilla
 Course: Object-Oriented Programming
 Repository used: BobsRentals_EW
 ========================================
-
 ========================================
 #1. Start up
 ========================================
-The application starts by asking the amount of skis and snowboards currently available for rent and adds it into the RentalShop class.
+The application starts by adding the starting amount of equipment before proceeding to the main menu
 
 ========================================
-#2. The main menu
+#2. Main menu
 ========================================
 This gives the user to choose to add rentals, process returns, Keep track of inventory, 
 and an end of day report for the total amount of equipment rented alongside the daily revenue.
 
+*The prompts have been changed to numbers to keep it consistent when selecting a menu in contrast to
+ typing the name of the menu.
 ========================================
 #3. New Customer Rental
 ========================================
-This will first process the customer's name and ID. Then, it will first ask which rental period to choose. 
-Afterwards, it will ask for either skis or snowboards and then the quantity. 
-*it will ask if finished, however, you could type N and loop through more equipment is being rented.
+This will first process the customer's name, ID, and which rental period to choose. 
+Afterwards, it will ask for either skis or snowboards and then brand which then adds them into the "rental_shop" class.
+The amount of inventory will not be subtracted until the rental is complete. 
 
+*variables in this function (example: hourly_rental and customer_obj) are often delcared as global to help carry over info and process returns.
 ========================================
 #4. Rental Return
 ========================================
+This will process returns for any existing customer that would return any equipment rented. 
+It will ask for the name and ID of the customer to search in the database for the matching info.
+This keeps track of the revenue in the shop from every return completed.
 
 ========================================
 #5. Show Inventory
 ========================================
 This shows the available inventory for the day. 
-*This number will decrease as more rentals are made.
+*The amounts will decrease and increase depending on rentals and returns.
 
 ========================================
 #6. End of Day
